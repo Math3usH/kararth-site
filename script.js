c25e90df855d714cd3a6da83951625abe32b000d
DanielVasc <estudante.danielvasc@gmail.com>
2023-11-22 13:11:16 -0300

    é isso msm

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function toggleList(){
     var list = document.getElementById("list");
     list.classList.toggle("hidden");
 }
-function toggleform() {
+function toggleForm() {
     var form = document.getElementById("myForm");
     form.classList.toggle("hidden");
+
+    var heading = document.getElementById("toggleHeading");
+    heading.textContent = form.classList.contains("hidden") ? "agende seu horario" : "agende seu horario";
 }
