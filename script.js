7ab64c0a350bf860adadbcbccef2c2e478449f68
Math3usH <matheushenriquecalsavara@gmail.com>
2023-11-22 09:32:10 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleList(){
+    var list = document.getElementById("list");
+    list.classList.toggle("hidden");
+}
+function toggleform() {
+    var form = document.getElementById("myForm");
+    form.classList.toggle("hidden");
+}
