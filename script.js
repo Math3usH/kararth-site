2156adb91d912c3516a93c74dced89bf407581bd
DanielVasc <estudante.danielvasc@gmail.com>
2023-11-29 09:31:14 -0300

    novo

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function toggleList(){
-    var list = document.getElementById("list");
-    list.classList.toggle("hidden");
+function toggleform() {
+    var form = document.getElementById("myForm");
+    form.classList.toggle("hidden");
 }
-
