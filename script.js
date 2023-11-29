288d6f86d326a08a6f4016b07e0a8cf5a8fbe1a2
DanielVasc <131368401+DanielVasc@users.noreply.github.com>
2023-11-29 14:33:19 -0300

    go

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function toggleform() {
-    var form = document.getElementById("myForm");
-    form.classList.toggle("hidden");
-}
+    document.addEventListener('DOMContentLoaded', function () {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+        checkboxes.forEach(function (checkbox) {
+            checkbox.addEventListener('change', function () {
+                var total = 0;
+                checkboxes.forEach(function (cb) {
+                    if (cb.checked) {
+                        total += parseInt(cb.getAttribute('data-valor')) || 0;
+                    }
+                });
+
+                document.getElementById('total').textContent = 'Total: R$' + total;
+            });
+        });
+    });
